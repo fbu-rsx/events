cf8b8e913aafe16ebd1c03d2bbb2090994464bd6
chenxiuqi <xiuqichen8@gmail.com>
2017-07-10 16:55:39 -0700

    Changed location of map view to Menlo Park, CA

--- events/ViewController.swift
+++ events/ViewController.swift
@@ -16,15 +16,15 @@ class ViewController: UIViewController {
         
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: 37.48, longitude: -122.14, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.position = CLLocationCoordinate2D(latitude: 37.48, longitude: -122.14)
+        marker.title = "Menlo Park"
+        marker.snippet = "California"
         marker.map = mapView
     }
 
