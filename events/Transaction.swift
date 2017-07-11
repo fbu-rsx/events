866837eef9c891afde657c2d39e2f6e42febb1a2
Rhian Chavez <rhianchavez11@fb.com>
2017-07-11 13:14:50 -0700

    added visaDirect Manager & transaction class

---
+++ events/Transaction.swift
@@ -0,0 +1,13 @@
+//
+//  Transaction.swift
+//  events
+//
+//  Created by Rhian Chavez on 7/11/17.
+//  Copyright © 2017 fbu-rsx. All rights reserved.
+//
+
+import Foundation
+
+class Transaction{
+    
+}
---
+++ events/VisaDirectAPIManager.swift
@@ -0,0 +1,37 @@
+//
+//  VisaDirectAPIManager.swift
+//  events
+//
+//  Created by Rhian Chavez on 7/11/17.
+//  Copyright © 2017 fbu-rsx. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class VisaDirectAPIManager{
+    
+    func pullFundsTransacrtions(){
+        
+    }
+    
+    func multiPullFuncsTransactions(){
+        
+    }
+    
+    func pushFundsTransactions(){
+        
+    }
+    
+    func multiPushFundsTransactions(){
+        
+    }
+    
+    func reverseFundsTransactions(){
+        
+    }
+    
+    func multiReverseFundsTransactions(){
+        
+    }
+}
