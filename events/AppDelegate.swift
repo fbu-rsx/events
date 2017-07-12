50651d2475f7a7c3956b64d334ae5fe9b5485814
Rhian Chavez <rhianchavez11@fb.com>
2017-07-12 10:42:00 -0700

    certs

--- events/AppDelegate.swift
+++ events/AppDelegate.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 import GoogleMaps
-import OAuthSwift
+import Alamofire
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -54,7 +54,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
--- events/VisaDirectAPIManager.swift
+++ events/VisaDirectAPIManager.swift
@@ -11,7 +11,35 @@ import Alamofire
 
 class VisaDirectAPIManager{
     
+    static var serverTrustPolicies: [String: ServerTrustPolicy] = ["your-domain.com": .disableEvaluation]
+    
+    let sessionManager = Alamofire.SessionManager(
+        serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies)
+    )
+    
+    let cert = PKCS12.init(mainBundleResource: "cert", resourceType: "p12", password: "Temp2509!");
+    
+    init(){
+        self.sessionManager.delegate.sessionDidReceiveChallenge = { session, challenge in
+            if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodClientCertificate {
+                return (URLSession.AuthChallengeDisposition.useCredential, self.cert.urlCredential());
+            }
+            if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
+                return (URLSession.AuthChallengeDisposition.useCredential, URLCredential(trust: challenge.protectionSpace.serverTrust!));
+            }
+            return (URLSession.AuthChallengeDisposition.performDefaultHandling, Optional.none);
+        }
+    }
+    
+    ///////////////////////////////////////////////////
+    
+    let userid = "L9MV1KNNDP6VZXN22U5W21ySfRLChcc9tCm36iALeiQaLD2Og"
+    let password = "sO6F2Xgmu4sW1lIl"
+
+ 
     func pullFundsTransacrtions(){
+        //let urlString = "https://sandbox.api.visa.com/visadirect/fundstransfer/v1/pullfundstransactions"
+        
         
     }
     
