ecc7d48dfe66aaf13d4774fe71faed6d3e0b4bf0
chenxiuqi <xiuqichen8@gmail.com>
2017-07-13 15:48:49 -0700

    Added maps features

--- events/Utilities.swift
+++ events/Utilities.swift
@@ -5,5 +5,24 @@
 //  Created by Xiu Chen on 7/13/17.
 //  Copyright © 2017 fbu-rsx. All rights reserved.
 //
+import UIKit
+import MapKit
 
-import Foundation
+// MAPS: Helper Extensions
+extension UIViewController {
+    func showAlert(withTitle title: String?, message: String?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+extension MKMapView {
+    func zoomToUserLocation() {
+        guard let coordinate = userLocation.location?.coordinate else { return }
+        let region = MKCoordinateRegionMakeWithDistance(coordinate, 10000, 10000)
+        setRegion(region, animated: true)
+        print("in utilities")
+    }
+}
