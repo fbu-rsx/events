9dba05bed4108992c5c32295b6a183df8beb85ec
Rhian Chavez <rhianchavez11@fb.com>
2017-07-13 15:50:22 -0700

    UI

---
+++ events/ImageCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCollectionViewCell.swift
+//  events
+//
+//  Created by Rhian Chavez on 7/13/17.
+//  Copyright © 2017 fbu-rsx. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
