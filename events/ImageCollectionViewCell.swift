2b27f75e0026c437b1a040e2fa68de6fc5cc62fb
Rhian Chavez <rhianchavez11@fb.com>
2017-07-13 15:54:05 -0700

    merge

--- events/ImageCollectionViewCell.swift
+++ events/ImageCollectionViewCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ImageCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
