671b2a67e764412bd34d8e5f86a7e270d59742a9
Rhian Chavez <rhianchavez11@fb.com>
2017-07-13 10:28:26 -0700

    pull

---
+++ events/EventsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventsTableViewCell.swift
+//  events
+//
+//  Created by Rhian Chavez on 7/13/17.
+//  Copyright © 2017 fbu-rsx. All rights reserved.
+//
+
+import UIKit
+
+class EventsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
