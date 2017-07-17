a37c3cf9eaf10bd642b4befdde63741a0cf150e0
Rhian Chavez <rhianchavez11@fb.com>
2017-07-17 13:44:37 -0700

    merge

--- events/DetailedEventViewController.swift
+++ events/DetailedEventViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import ImagePicker
 
-class DetailedEventViewController: UIViewController {
+class DetailedEventViewController: UIViewController, ImagePickerDelegate {
+    
+    let imagePickerController = ImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        imagePickerController.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +26,24 @@ class DetailedEventViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func upload(_ sender: UIButton) {
+        present(imagePickerController, animated: true, completion: nil)
+    }
+
+    func wrapperDidPress(_ imagePicker: ImagePickerController, images: [UIImage]){
+        
+    }
+    
+    func doneButtonDidPress(_ imagePicker: ImagePickerController, images: [UIImage]){
+        for image in stride(from: 0, to: images.count, by: 1){
+            FirebaseStorageManager.shared.uploadImage(imageID: String(image), image: images[image], completion: {})
+        }
+        imagePickerController.dismiss(animated: true, completion: nil)
+    }
+    
+    func cancelButtonDidPress(_ imagePicker: ImagePickerController){
+        
+    }
 
     /*
     // MARK: - Navigation
