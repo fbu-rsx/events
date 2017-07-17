8bdafbf42c462af1d5a3c6bf25485cfff240f7c2
Rhian Chavez <rhianchavez11@fb.com>
2017-07-17 16:12:41 -0700

    Detail Page Updates

--- events/DetailedEventViewController.swift
+++ events/DetailedEventViewController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 import ImagePicker
+import MapKit
 
-class DetailedEventViewController: UIViewController, ImagePickerDelegate {
+class DetailedEventViewController: UIViewController, ImagePickerDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var centerImage: UIImageView!
+    @IBOutlet weak var organizerLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var tableView: UITableView!
+    
+/*    var eventid: String
+ var eventname: String
+ var totalcost: Double? //optional because may just be a free event
+ var location: [Double]
+ var organizerID: String //uid of the organizer
+ var guestlist: [String: Bool]
+ var photos: [String: String]
+ var eventDictionary: [String: Any]*/
+ 
+    var event: Event?{
+        didSet{
+            let longitude = CLLocationDegrees(exactly: (event?.location[0])!)
+            let latitude = CLLocationDegrees(exactly: (event?.location[1])!)
+            let location = CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!)
+            mapView.setCenter(location, animated: true)
+            //centerImage.image = event?.organizerID
+            // set organizerlabel as well
+            
+        }
+    }
     
     let imagePickerController = ImagePickerController()
 
@@ -17,8 +45,14 @@ class DetailedEventViewController: UIViewController, ImagePickerDelegate {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
         imagePickerController.delegate = self
+        tableView.delegate = self
+        collectionView.delegate = self
+        
+        let bundle = Bundle(path: "/Users/rhianchavez11/Documents/events/events/Views")
+        collectionView.register(UINib(nibName: "ImageCollectionViewCell", bundle: bundle), forCellWithReuseIdentifier: "customImageCell")
+        tableView.register(UINib(nibName: "userTableViewCell", bundle: bundle), forCellReuseIdentifier: "userCell")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,6 +78,27 @@ class DetailedEventViewController: UIViewController, ImagePickerDelegate {
     func cancelButtonDidPress(_ imagePicker: ImagePickerController){
         
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (event?.guestlist.count)!
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        return (event?.photos.count)!
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customImageCell", for: indexPath) as! ImageCollectionViewCell
+        // cell.imageView.image = event?.photos
+        return cell
+    }
+    
+    
 
     /*
     // MARK: - Navigation
---
+++ events/userTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  userTableViewCell.swift
+//  events
+//
+//  Created by Rhian Chavez on 7/17/17.
+//  Copyright © 2017 fbu-rsx. All rights reserved.
+//
+
+import UIKit
+
+class userTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
