281eeea4fb7e0a12f20f4d4726ba7ebc1f8add70
chenxiuqi <xiuqichen8@gmail.com>
2017-07-17 17:48:38 -0700

    Added to create and event flow: map, time, title

---
+++ events/CreateEventGuestsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CreateEventGuestsViewController.swift
+//  events
+//
+//  Created by Xiu Chen on 7/17/17.
+//  Copyright © 2017 fbu-rsx. All rights reserved.
+//
+
+import UIKit
+
+class CreateEventGuestsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(hexString: "#e74c3c")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
