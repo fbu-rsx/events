360b6d4642d82ef0376c146782d1df7184a0f37d
chenxiuqi <xiuqichen8@gmail.com>
2017-07-21 09:11:45 -0700

    added tabbar controller

--- Pods/Target Support Files/Pods-events/Pods-events-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_events : NSObject
-@end
-@implementation PodsDummy_Pods_events
-@end
--- Pods/Target Support Files/SimpleTab/SimpleTab-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SimpleTab : NSObject
-@end
-@implementation PodsDummy_SimpleTab
-@end
