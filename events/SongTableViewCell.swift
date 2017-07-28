41d8d0cf0554504d2819aa6adeb1805b37e5a364
Rhian Chavez <rhianchavez11@fb.com>
2017-07-28 11:15:59 -0700

    spotify updates

---
+++ events/SongTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SongTableViewCell.swift
+//  events
+//
+//  Created by Rhian Chavez on 7/28/17.
+//  Copyright © 2017 fbu-rsx. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
